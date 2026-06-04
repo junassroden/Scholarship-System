ad4059cd9fdd449f2a73a3b0b3e9091b8d345c6c
Jhonas Cabanero <cabanerojhoas@gmail.com>
2026-06-04 11:22:24 +0800

    Add files via upload

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * App configurations
+ */
+
+//Dev or Prod
+define('IS_DEV', true);
+
+//Database Config
+define('DB_DRIVER', 'mysql');
+define('DB_HOST', 'sql100.ezyro.com');
+define('DB_PORT', '3306');
+define('DB_NAME', 'ezyro_41678832_dbscholarshipsys');
+define('DB_USERNAME', 'ezyro_41678832');
+define('DB_PASSWORD', 'ed798fb36f7cc');
+define('DB_CHARSET', 'utf8mb4');
+define('DB_PREFIX', '');
+define('DB_PATH', '');
+
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "app/views/**/*.{html,js,php}",
+    "app/forms/**/*.{html,js,php}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
